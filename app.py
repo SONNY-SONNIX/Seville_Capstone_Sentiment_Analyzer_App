00ca1a9fffcdec4a4161157e970877aa3ee24917
Sonny Agorvor-Otchie <Sonny4Sonnix@users.noreply.huggingface.co>
2023-09-10 19:44:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,19 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 sentiment = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
-
-
-
-
-
-
+def get_sentiment(input_text):
+    result = sentiment(input_text)
+    sentiment_label = result[0]['label']
+    sentiment_score = result[0]['score']
+    
+    if sentiment_label == 'LABEL_1':
+        sentiment_label = "positive"
+    elif sentiment_label == 'LABEL_0':
+        sentiment_label = "neutral"
+    else:
+        sentiment_label = "negative"
+    
+    return f"Sentiment: {sentiment_label.capitalize()}, Score: {sentiment_score:.2f}"
 
 
 iface = gr.Interface(fn=get_sentiment,title="Sentimental Analysis", inputs="text",outputs="text")
