e4705fca6c33e8e3ab9e2302050ce97cb0ee53e7
Sonny Agorvor-Otchie <Sonny4Sonnix@users.noreply.huggingface.co>
2023-09-10 19:42:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import gradio as gr
 import transformers as pipeline 
 from transformers import AutoTokenizer,AutoModelForSequenceClassification
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+
+model_name = "Sonny4Sonnix/Roberta-capstone_2"  # Replace with the name of the pre-trained model you want to use
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
