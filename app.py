dd1aa2cbd7a4856703d912de15e0debc0074bf46
Sonny Agorvor-Otchie <Sonny4Sonnix@users.noreply.huggingface.co>
2023-09-10 19:40:49 +0000

    Update app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import gradio as gr 
+import transformers as pipeline 
+from transformers import AutoTokenizer,AutoModelForSequenceClassification
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
